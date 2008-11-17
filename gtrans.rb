dc93eda03aa33afcb3b878783145defc3a15e4b4
gom <gomgom68@gmail.com>
2008-11-18 00:08:50 +0900

    Add RDoc comment gtrans.rb

--- gtrans.rb
+++ gtrans.rb
@@ -2,31 +2,34 @@
 require 'rubygems'
 require 'rtranslate'
 
-# execute Google Transrate with rtranslate
+#
+#= wrapper class for rtranslate
+# Translate sentences with googletransrate
+#
+#Authors:: gom
+#
 class GTrans
+  #
+  # define to use language
+  #
   Mode = {:jp=>Language::JAPANESE,
           :en=>Language::ENGLISH}
 
-  # execute a transration
-  # s: string
-  # from: language for 
-  # to: language
-  def trans s,from,to # 翻訳
+  # ==execute transration
+  #
+  # [sentence] sentence that want to translate
+  # [from] language
+  # [to] language
+  #
+  # [return] translated sentences
+  #
+  def self.trans sentence,from,to
     begin
-      @from = Mode[from.to_sym]
-      @to = Mode[to.to_sym]
-    rescue
+      from = Mode[from.to_sym]
+      to = Mode[to.to_sym]
+    rescue => e
       return "Error: no Transrate Mode!"
     end
-    result = Translate.t(s, @from, @to)
+    result = Translate.t(sentence, from, to)
   end
 end
-
-# entry
-# ARGV[0]=from, [1]=to, [2]=sentence
-#from = ARGV.shift
-#to = ARGV.shift
-#sentence = ARGV.shift
-
-#t = GTrans.new
-#puts t.trans(sentence,from,to)
