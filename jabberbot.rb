334cca3d58ef559e7f4af808a1cb9f3f2e3ab146
gom <gomgom68@gmail.com>
2008-11-18 00:12:01 +0900

    modified jabberbot, unite wassr_transbot and runbot.rb

--- jabberbot.rb
+++ jabberbot.rb
@@ -1,33 +1,119 @@
 # -*- coding: utf-8 -*-
 require "rubygems"
 require "xmpp4r-simple"
+require "webrick"
 
+#
+#= Jabber Client for auto messaging
+# automatic responce to the message
+#
 class JabberBot
-  ABORT_MSG = "aborting..."
-  
+  # Error Messages
+  CONNECTOR_ERR = "I can't make the connect: "
+  RECEIVER_ERR = "I can't receive the message: "
+  DELIVER_ERR = "I can't delivery the message: "
+  DISCONNECTOR_ERR = "I can't leave the server...: "
+
+  LOG_FILE = "received.log"
+
+  #
+  #===Constractor
+  # connect to Jabber Server, add callback process
+  #[user] account name for connecting jabber
+  #[pass] the account's password
+  #
   def initialize user,pass
-    @client = Jabber::Simple.new(user,pass)
-    Thread.start {
-      @client.client.add_message_callback {|msg|
-        self.process msg
-      }
+    begin
+      @client = Jabber::Simple.new(user,pass)
+    rescue => e
+      raise CONNECTOR_ERR + e
+    end
+    
+    @client.client.add_message_callback {|msg|
+      begin
+        self.receiver msg
+      rescue => e
+        self.logging "[#{Time.now}]#{$0}: #{e.class} > #{e}"
+        raise RECEIVER_ERR + e
+      end
     }
   end
 
-  def process msg
-    # implement at child
+  #
+  #=== Start JabberBot with WEBrick Daemon
+  #
+  def start
+    begin
+      WEBrick::Daemon.start {
+        loop {
+          self.check
+          sleep 10
+        }
+      }
+    rescue => e
+      self.logging "[#{Time.now}]#{$0}: #{e}"
+    end
   end
-
-  def close from
-    @client.deliver(from, ABORT_MSG)
-    @client.disconnect
-    puts ABORT_MSG + Time.now.to_s
-    Thread.exit
-  end
-
+  
+  #
+  #=== Connecting Check
+  # if client is disconnected, try reconnecting.
+  #
   def check
-    @client.reconnect unless @client.connected?
+    begin
+      unless @client.connected?
+        self.logging "[#{Time.now}]Disconnected! Try Reconnecting!"
+        @client.reconnect
+      end
+      @client.status(nil, "I'm alive")
+    rescue => e
+      raise CONNECTOR_ERR + e
+    end
   end
 
-  protected :close, :process
+  protected
+  #
+  #=== Deliver a message to the client that sent message
+  #[sent] client from the message
+  #[msg] the message for delivering
+  #
+  def deliver sent,msg
+    begin
+      @client.deliver(sent, msg)
+    rescue => e
+      raise DELIVER_ERR + e
+    end
+  end
+
+  #
+  #=== Action for receiving messages
+  #This method is only definition.
+  #Inheritances can inplement this method.
+  #
+  #[msg] a received message
+  #
+  def receiver msg
+    self.logging "received #{msg.body} from #{msg.from}"
+  end
+
+  #
+  #=== Disconnecting jabber server
+  #
+  def close
+    begin
+      @client.disconnect
+    rescue => e
+      raise DISCONNECTOR_ERR + e
+    end
+  end
+
+  #
+  #=== logging any message
+  #[msg] a logged message
+  #
+  def logging msg
+    File.open(LOG_FILE, "a"){|f|
+      f.puts "[#{Time.now}] #{$0}: #{msg}"
+    }
+  end
 end
